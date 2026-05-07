ed319322e09b0b3be6c65da11d55caf159e23c89
Anand Mainali <anandmainali5@gmail.com>
2026-05-07 12:38:12 +0545

    SignIn & SignOut added

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,5 +1,9 @@
-function SignInPage () {
-    return <></>
+import {SignIn} from "@clerk/clerk-react";
+
+function SignInPage() {
+    return <div className={"auth-container"}>
+        <SignIn routing={"path"} path={"/sign-in"} signUpUrl={"/sign-up"}/>
+    </div>
 }
 
 export default SignInPage
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,5 +1,9 @@
-function SignUpPage () {
-    return <></>
+import {SignUp} from "@clerk/clerk-react"
+
+function SignUpPage() {
+    return <div className={"auth-container"}>
+        <SignUp routing={"path"} path={"/sign-up"} signInUrl={"/sign-in"}/>
+    </div>
 }
 
 export default SignUpPage
